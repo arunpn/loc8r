30e07faf54fc5eaad1108b7ff61b6b571849acd1
arunpn <oracle.arunp@gmail.com>
2016-06-28 15:07:30 -0700

    Chapter 4 end

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-router.get('/',ctrlMain.index);
+// Location Pages
+router.get('/',ctrlLocations.homelist);
+router.get('/location/',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+// Other Pages
+router.get('/about',ctrlOthers.about);
 
 module.exports = router;
